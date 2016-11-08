3982ec2f57e2011130e27778dab397585b9165d1
zhha <zhanghao.dev@gmail.com>
2016-11-08 16:29:27 +0800

    在Alamofire 4.0.1上去掉了一些iOS9的新特性

--- Source/Request.swift
+++ Source/Request.swift
@@ -577,27 +577,28 @@ open class UploadRequest: DataRequest {
 
 // MARK: -
 
-#if !os(watchOS)
-
-/// Specific type of `Request` that manages an underlying `URLSessionStreamTask`.
-open class StreamRequest: Request {
-    enum Streamable: TaskConvertible {
-        case stream(hostName: String, port: Int)
-        case netService(NetService)
-
-        func task(session: URLSession, adapter: RequestAdapter?, queue: DispatchQueue) throws -> URLSessionTask {
-            let task: URLSessionTask
-
-            switch self {
-            case let .stream(hostName, port):
-                task = queue.syncResult { session.streamTask(withHostName: hostName, port: port) }
-            case let .netService(netService):
-                task = queue.syncResult { session.streamTask(with: netService) }
-            }
-
-            return task
-        }
-    }
-}
-
-#endif
+// MARK: 适配iOS 8
+//#if !os(watchOS)
+//
+///// Specific type of `Request` that manages an underlying `URLSessionStreamTask`.
+//open class StreamRequest: Request {
+//    enum Streamable: TaskConvertible {
+//        case stream(hostName: String, port: Int)
+//        case netService(NetService)
+//
+//        func task(session: URLSession, adapter: RequestAdapter?, queue: DispatchQueue) throws -> URLSessionTask {
+//            let task: URLSessionTask
+//
+//            switch self {
+//            case let .stream(hostName, port):
+//                task = queue.syncResult { session.streamTask(withHostName: hostName, port: port) }
+//            case let .netService(netService):
+//                task = queue.syncResult { session.streamTask(with: netService) }
+//            }
+//
+//            return task
+//        }
+//    }
+//}
+//
+//#endif
